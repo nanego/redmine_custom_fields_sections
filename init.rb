2031010f4d2693ddcf5a539e003943740cc94d8f
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-04-16 17:12:07 +0200

    Creating plugin (#89344)

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_custom_fields_sections do
+  name 'Redmine Custom Fields Sections plugin'
+  author 'Vincent Robert'
+  description 'This plugin gives possibility to group custom fields in sections.'
+  version '0.0.1'
+  url 'https://github.com/nanego/redmine_custom_fields_sections'
+  author_url 'https://github.com/nanego'
+end
