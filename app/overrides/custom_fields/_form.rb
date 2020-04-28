a5b2a0f70126d36ae7daf591b2eec24e5db15501
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-04-28 19:44:46 +0200

    Add Section to Project#form

--- app/overrides/custom_fields/_form.rb
+++ app/overrides/custom_fields/_form.rb
@@ -2,7 +2,7 @@
 
 # Add content in bottom of <div class="splitcontentright">
 Deface::Override.new :virtual_path  => "custom_fields/_form",
-                     :original      => 'f730bb275511ba2092ae2acfba22186918b93a51',
+                     :original      => "f730bb275511ba2092ae2acfba22186918b93a51",
                      :name          => "add_section_to_project_custom_fields",
                      :insert_bottom => "div.splitcontentright",
                      :text          => <<-EOS
---
+++ app/overrides/projects/_form.rb
@@ -0,0 +1,12 @@
+#encoding: utf-8
+
+# Remove existent for custom_fields
+# Deface::Override.new :virtual_path      => "projects/_form",
+#                      :original          => "785a6bfc5ed26ff7866c048622711da14149f8ae",
+#                      :name              => "replace_custom_fields_in_projects_form",
+#                      :remove            => "erb[loud]:contains('custom_field_tag_with_label')"
+
+# Deface::Override.new :virtual_path      => "projects/_form",
+#                      :name              => "remove_empty_p_in_projects_form",
+#                      :original          => "515bd358bb7d990930f0e2b3de399db1787a2567",
+#                      :remove            => "p:empty"
