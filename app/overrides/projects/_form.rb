7b6fd506fabc0c0082fb497ab3808dc540fbc9d6
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-04-29 11:17:08 +0200

    Make _form override with Deface and Hook

--- app/overrides/projects/_form.rb
+++ app/overrides/projects/_form.rb
@@ -1,12 +1,18 @@
 #encoding: utf-8
 
-# Remove existent for custom_fields
-# Deface::Override.new :virtual_path      => "projects/_form",
-#                      :original          => "785a6bfc5ed26ff7866c048622711da14149f8ae",
-#                      :name              => "replace_custom_fields_in_projects_form",
-#                      :remove            => "erb[loud]:contains('custom_field_tag_with_label')"
+# Remove existent custom_fields on projects/_form page
+Deface::Override.new :virtual_path      => "projects/_form",
+                     :original          => "785a6bfc5ed26ff7866c048622711da14149f8ae",
+                     :name              => "remove_custom_fields_generation_in_projects_form",
+                     :remove            => "erb[loud]:contains('custom_field_tag_with_label')"
 
+Deface::Override.new :virtual_path      => "projects/_form",
+                     :name              => "remove_empty_p_in_projects_form",
+                     :original          => "515bd358bb7d990930f0e2b3de399db1787a2567",
+                     :remove            => "p:empty"
+
+# Add custom code to add Section to custom_fields
 # Deface::Override.new :virtual_path      => "projects/_form",
-#                      :name              => "remove_empty_p_in_projects_form",
-#                      :original          => "515bd358bb7d990930f0e2b3de399db1787a2567",
-#                      :remove            => "p:empty"
+#                      :name              => "add_custom_fields_with_section_in_projects_form",
+#                      :insert_bottom     => "div#tab-content-info form div.tabular:first-child",
+#                      :partial           => "projects/_section_custom_fields"
--- lib/redmine_custom_fields_sections/hooks.rb
+++ lib/redmine_custom_fields_sections/hooks.rb
@@ -5,5 +5,7 @@ module RedmineCustomFieldsSections
     def view_layouts_base_html_head(context)
       stylesheet_link_tag("section_custom_fields", :plugin => "redmine_custom_fields_sections")
     end
+
+    render_on :view_projects_form, :partial => "projects/section_custom_fields"
   end
 end
