c1d64d59427d3ca438ffe54d3bc3a6dff40781b3
Nicolas Brousse <n.brousse@pantographe.studio>
2020-05-05 17:50:10 +0200

    test: add test to ProjectCustomField patch

--- spec/models/custom_fields_section_spec.rb
+++ spec/models/custom_fields_section_spec.rb
@@ -6,31 +6,31 @@ describe CustomFieldsSection do
 
   describe "create and destroy" do
     it "creates a section with a name" do
-      section = CustomFieldsSection.new(name: "Test section")
+      section = described_class.new(name: "Test section")
       expect(section.save).to eq true
     end
 
     it "doesn't create a section without a name" do
-      section = CustomFieldsSection.new()
+      section = described_class.new()
       expect(section.save).to eq false
     end
 
     it "doesn't create a section with a not unique name" do
-      CustomFieldsSection.create(name: "Test section")
+      described_class.create(name: "Test section")
 
-      section = CustomFieldsSection.new(name: "Test section")
+      section = described_class.new(name: "Test section")
       expect(section.save).to eq false
     end
 
     it "destroys a record" do
-      section = CustomFieldsSection.new(name: "Test section")
+      section = described_class.new(name: "Test section")
       expect(section.destroy).to eq section
     end
   end
 
   describe "add projects custom fields to section" do
     it "section to custom_fields" do
-      section = CustomFieldsSection.new(name: "Test section")
+      section = described_class.new(name: "Test section")
       custom_fields(:custom_fields_003).update(section: section)
 
       expect(section.project_custom_fields.first.name).to eq "Development status"
