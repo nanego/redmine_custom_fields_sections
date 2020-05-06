055c455d192f66834ef93e1e5677cab2e0371e42
Vincent Robert <vincent.robert@nanego.com>
2020-05-06 14:09:47 +0200

    Only create table if it does not already exist

--- db/migrate/20200417100900_create_custom_fields_sections.rb
+++ db/migrate/20200417100900_create_custom_fields_sections.rb
@@ -3,7 +3,7 @@ class CreateCustomFieldsSections < ActiveRecord::Migration[5.2]
     create_table :custom_fields_sections do |t|
       t.string :name, :null => false
       t.datetime :created_on, :null => false
-    end
+    end unless ActiveRecord::Base.connection.table_exists? 'custom_fields_sections'
 
     add_reference :custom_fields, :section, :index => true, :foreign_key => { to_table: :custom_fields_sections }
   end
