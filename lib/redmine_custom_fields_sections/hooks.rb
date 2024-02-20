aae3d6c5dd562812b35e181c568a0f9779ae05e8
Yalaeddin <y.alaeddin@adacis.net>
2024-02-20 15:42:27 +0100

    ⬆️ [Plugin CustomFields Sections] Compatibilité avec Redmine 5.1.1

--- lib/redmine_custom_fields_sections/hooks.rb
+++ lib/redmine_custom_fields_sections/hooks.rb
@@ -1,11 +1,19 @@
 module RedmineCustomFieldsSections
-  class Hooks < Redmine::Hook::ViewListener
+  module Hooks
+    class Hooks < Redmine::Hook::ViewListener
+      # Add our css/js on each page
+      def view_layouts_base_html_head(context)
+        stylesheet_link_tag("section_custom_fields", plugin: "redmine_custom_fields_sections")
+      end
 
-    # Add our css/js on each page
-    def view_layouts_base_html_head(context)
-      stylesheet_link_tag("section_custom_fields", plugin: "redmine_custom_fields_sections")
+      render_on :view_projects_form, partial: "projects/section_custom_fields"
     end
 
-    render_on :view_projects_form, partial: "projects/section_custom_fields"
+    class ModelHook < Redmine::Hook::Listener
+      def after_plugins_loaded(_context = {})
+        require_relative "models/project_custom_field_patch"
+        require_relative "helpers/custom_fields_helper_patch"
+      end
+    end
   end
 end
---
+++ lib/redmine_custom_fields_sections/models/project_custom_field_patch.rb
@@ -0,0 +1,15 @@
+require_dependency "project_custom_field"
+
+module RedmineCustomFieldsSections::Models
+  module ProjectCustomFieldPatch
+    def self.included(base)
+      base.class_eval do
+        belongs_to :section, class_name: "CustomFieldsSection", optional: true
+
+        safe_attributes("section_id")
+      end
+    end
+  end
+end
+
+ProjectCustomField.send(:include, RedmineCustomFieldsSections::Models::ProjectCustomFieldPatch)
